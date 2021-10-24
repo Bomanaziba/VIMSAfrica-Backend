86416d39df58d132e0f8a35b580c826b4a1460e0
adebortimothy <adebortimothy@gmail.com>
2021-10-24 16:47:16 +0100

    .

--- VIMSAfrica.CORE/Model/IVehicle.cs
+++ VIMSAfrica.CORE/Model/IVehicle.cs
@@ -8,5 +8,17 @@ namespace VIMSAfrica.CORE.Model
 {
     public interface IVehicle
     {
+         int Id { get; set; }
+         string RegistrationNumber { get; set; }
+         int VehicleBrandId { get; set; }
+         int VehicleModelId { get; set; }
+         int ModelYearId { get; set; }
+         string Vin { get; set; }
+         DateTime VehicleLicenseExpiry { get; set; }
+         DateTime MotExpiry { get; set; }
+         DateTime DateCreated { get; set; }
+         DateTime InsuranceExpiry { get; set; }
+         string VehicleOwnerName { get; set; }
+         string Mileage { get; set; }
     }
 }
--- VIMSAfrica.CORE/Model/Impl/Vehicle.cs
+++ VIMSAfrica.CORE/Model/Impl/Vehicle.cs
@@ -8,5 +8,17 @@ namespace VIMSAfrica.CORE.Model.Impl
 {
     public class Vehicle:IVehicle
     {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public int VehicleBrandId { get; set; }
+        public int VehicleModelId { get; set; }
+        public int ModelYearId { get; set; }
+        public string Vin { get; set; }
+        public DateTime VehicleLicenseExpiry { get; set; }
+        public DateTime MotExpiry { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime InsuranceExpiry { get; set; }
+        public string VehicleOwnerName { get; set; }
+        public string Mileage { get; set; }
     }
 }
